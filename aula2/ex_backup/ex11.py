d4f19ff32096e0cb0109ce585f22762ad3e79c13
Marcelo <marcelorcarnauba@gmail.com>
2023-08-22 18:49:36 -0300

    Novas alterações

---
+++ aula2/ex_backup/ex11.py
@@ -0,0 +1,10 @@
+salario = float(input("Digite seu salário: "))
+
+if (salario <= 280):
+    print(f'Anterior: {salario}, Aumento: 20% - {salario * 0.20}, Novo: {salario +  0.20 * salario}')
+elif (salario <= 700):
+    print(f'Anterior: {salario}, Aumento: 15% - {salario * 0.15}, Novo: {salario +  0.15 * salario}')
+elif (salario <= 1500):
+    print(f'Anterior: {salario}, Aumento: 10% - {salario * 0.10}, Novo: {salario +  0.10 * salario}')
+else:
+    print(f'Anterior: {salario}, Aumento: 5% - {salario * 0.05}, Novo: {salario +  0.05 * salario}')
